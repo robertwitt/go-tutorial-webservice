aad802c997593c8a38f1269fe9c85db689a64a20
Robert Witt <robert.witt@sap.com>
2019-12-15 13:36:44 +0100

    User model

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/robertwitt/go-tutorial-webservice/models"
 )
 
 func main() {
-	fmt.Println("Hello from a module, Gophers!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Tricia",
+		LastName:  "McMillan",
+	}
+	fmt.Println(u)
 }
